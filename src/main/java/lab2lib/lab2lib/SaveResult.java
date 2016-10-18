3965bc0fc074412f9d3d5ea6db76fa18739b23f7
Karol Kucharski <187746@edu.p.lodz.pl>
2016-10-18 12:45:12 +0200

    Konstruktor inicjalizujacy SaveResult.

--- src/main/java/lab2lib/lab2lib/SaveResult.java
+++ src/main/java/lab2lib/lab2lib/SaveResult.java
@@ -7,4 +7,13 @@ public class SaveResult {
 	private boolean success;
 	
 	public SaveResult() { }
+
+	public SaveResult(String fileName, String correctMsg, String incorrectMsg, boolean success) {
+		super();
+		this.fileName = fileName;
+		this.correctMsg = correctMsg;
+		this.incorrectMsg = incorrectMsg;
+		this.success = success;
+	}
+	
 }
