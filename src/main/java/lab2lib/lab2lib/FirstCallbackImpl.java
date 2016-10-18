7c6c5466c44aa236dc42e7446b2ee39fd301d96a
Karol Kucharski <187746@edu.p.lodz.pl>
2016-10-18 12:45:12 +0200

    Pole scanner w FirstCallbackImpl

--- src/main/java/lab2lib/lab2lib/FirstCallbackImpl.java
+++ src/main/java/lab2lib/lab2lib/FirstCallbackImpl.java
@@ -1,6 +1,10 @@
 package lab2lib.lab2lib;
 
+import java.util.Scanner;
+
 public class FirstCallbackImpl implements FirstCallback {
+	
+	private Scanner scanner;
 
 	@Override
 	public String callback(SecondCallback callback) {
