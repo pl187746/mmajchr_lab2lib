57c5a21db99fd15009255c9fb1556ced9bee1046
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 16:48:38 +0100

    FirstCallbackImpl uzywa promptYesNo

--- src/main/java/lab2lib/lab2lib/FirstCallbackImpl.java
+++ src/main/java/lab2lib/lab2lib/FirstCallbackImpl.java
@@ -24,8 +24,7 @@ public class FirstCallbackImpl implements FirstCallback {
 			return null;
 		}
 		while(true) {
-			String c = ui.prompt("Czy chcesz zapisac plik pod inna nazwa? [T/N] ");
-			if(c.startsWith("T") || c.startsWith("t")) {
+			if(ui.promptYesNo("Czy chcesz zapisac plik pod inna nazwa?")) {
 				ui.show("Poprzednie ustawienia: ");
 				ui.show("Nazwa pliku: " + result.getFileName());
 				ui.show("String powodzenia: " + result.getCorrectMsg());
@@ -33,7 +32,7 @@ public class FirstCallbackImpl implements FirstCallback {
 				ui.show("Nowa nazwa pliku: ");
 				String newFileName = ui.prompt("Nowa nazwa pliku: ");
 				return newFileName;
-			} else if(c.startsWith("N") || c.startsWith("n")) {
+			} else {
 				return null;
 			}
 		}
