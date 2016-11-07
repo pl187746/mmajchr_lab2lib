4adbdb6833a94b6a230e85f09c26b802ff6cf755
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-07 16:36:47 +0100

    Pole SimpleUserInterface ui w OnCloseCallbackImpl oraz konstruktor OnCloseCallbackImpl.

--- src/main/java/lab2lib/lab2lib/OnCloseCallbackImpl.java
+++ src/main/java/lab2lib/lab2lib/OnCloseCallbackImpl.java
@@ -1,6 +1,12 @@
 package lab2lib.lab2lib;
 
 public class OnCloseCallbackImpl implements OnCloseCallback {
+	
+	private SimpleUserInterface ui;
+	
+	public OnCloseCallbackImpl(SimpleUserInterface ui) {
+		this.ui = ui;
+	}
 
 	@Override
 	public boolean closing() {
