ccdf75418c46e1e8b2ae6f16c1f37ff6ad7cb3d1
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-19 00:12:14 +0100

    Dodanie skryptu commitujacego wynik kompilacji

---
+++ after.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf depbin
+git clone --branch=bin --depth=5 https://pl187746:$ASDF@github.com/pl187746/mmajchr_lab2lib depbin >> /dev/null 2>> /dev/null
+cd depbin
+rm -rf target
+mkdir target
+cp -f ../target/*.jar ./target/
+git config --local user.name "Karol Kucharski"
+git config --local user.email 187746@edu.p.lodz.pl
+git add -A
+git commit -m "automatyczny build"
+git push -f origin bin >> /dev/null 2>> /dev/null
+cd ..
+rm -rf depbin
+echo "Build commited"
